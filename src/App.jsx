7e6766b0d8cb185d2965e98824d63d45fac0d078
sanjay <sanjayshre4815@gmail.com>
2025-06-22 19:41:28 +0530

    first part of hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 // should do within the app once. scrolltrigger will allow us the trigger animation based on the scroll, splittext all us to break text into indiviual words for text animation.
 const App = () => {
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"/>
+    </main>
   )
 }
 
